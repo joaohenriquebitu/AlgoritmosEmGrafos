f2d2df82b5bb15b7a21127c65bd9210414f2b056
joaohenriquebitu <134982253+joaohenriquebitu@users.noreply.github.com>
2026-03-22 20:02:25 -0300

    adicionado esqueleto do algoritmo de belman-ford

--- main.cs
+++ main.cs
@@ -3,8 +3,50 @@ using System.Collections.Generic;
 
 public class Program
 {
-    public static void main()
+
+    // algoritmo de bellman-ford, retornando também o caminho por meio dos predecessores
+    private (int[] distancias, int[] predecessores) BellmanFord(int origem, bool usarGrafoReverso = false, /*grafo grafo*/)
+        {
+            // int n = grafo.numVertices
+            int[] dist = new int[n];
+            int[] pred = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                dist[i] = int.MaxValue / 2;
+                pred[i] = -1;
+            }
+            dist[origem] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                foreach (var u in adjacencias.Keys)
+                {
+                    foreach (var aresta in adjacencias[u])
+                    {
+                        int v = aresta.Destino;
+                        int peso = aresta.Peso;
+
+                        if (dist[u] + peso < dist[v])
+                        {
+                            dist[v] = dist[u] + peso;
+                            pred[v] = u;
+                        }
+                    }
+                }
+            }
+
+            return (dist, pred);
+        }
+    public static void Main()
     {
-      
+
+
+
+
+
+
+        
+      //
     }
 }

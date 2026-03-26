7f85d98cbfaeb5f061aaa7ca7e305cd6a4aad8e6
joaohenriquebitu <134982253+joaohenriquebitu@users.noreply.github.com>
2026-03-26 15:54:14 -0300

    Atualizado o Bellmanford, teste funcionando

--- main.cs
+++ main.cs
@@ -1,52 +1,73 @@
 using System;
 using System.Collections.Generic;
 
-public class Program
-{
+class E {
+    public int v, w; 
+}
 
-    // algoritmo de bellman-ford, retornando também o caminho por meio dos predecessores
-    private (int[] distancias, int[] predecessores) BellmanFord(int origem, bool usarGrafoReverso = false, /*grafo grafo*/)
-        {
-            // int n = grafo.numVertices
-            int[] dist = new int[n];
-            int[] pred = new int[n];
+class Grafo {
+    public Dictionary<int, List<E>> adj = new Dictionary<int, List<E>>();
+    public void Add(int u, int v, int w) {
+        if (!adj.ContainsKey(u)) adj[u] = new List<E>();
+        if (!adj.ContainsKey(v)) adj[v] = new List<E>();
+        adj[u].Add(new E { v = v, w = w });
+    }
+}
 
-            for (int i = 0; i < n; i++)
-            {
-                dist[i] = int.MaxValue / 2;
-                pred[i] = -1;
-            }
-            dist[origem] = 0;
+public class Program {
+    static float inf = float.PositiveInfinity;
 
-            for (int i = 1; i < n; i++)
-            {
-                foreach (var u in adjacencias.Keys)
-                {
-                    foreach (var aresta in adjacencias[u])
-                    {
-                        int v = aresta.Destino;
-                        int peso = aresta.Peso;
+    public static (float[] distancias, int[] predecessores) BellmanFord(Grafo g, int origem) {
+        int n = 0;
+        foreach (var k in g.adj.Keys) n = Math.Max(n, k + 1);
 
-                        if (dist[u] + peso < dist[v])
-                        {
-                            dist[v] = dist[u] + peso;
-                            pred[v] = u;
-                        }
+        float[] d = new float[n];
+        int[] p = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            d[i] = inf;
+            p[i] = -1;
+        }
+        d[origem] = 0;
+
+        for (int i = 1; i < n; i++) {
+            foreach (var u in g.adj.Keys) {
+                foreach (var aresta in g.adj[u]) {
+                    int v = aresta.v;
+                    int w = aresta.w;
+
+                    if (d[u] + w < d[v]) {
+                        d[v] = d[u] + w;
+                        p[v] = u;
                     }
                 }
             }
-
-            return (dist, pred);
         }
-    public static void Main()
-    {
 
+        foreach (var u in g.adj.Keys) {
+            foreach (var aresta in g.adj[u]) {
+                if (d[u] + aresta.w < d[aresta.v]) {
+                    Console.WriteLine("ciclo negativo detectado em " + u);
+                }
+            }
+        }
 
+        return (d, p);
+    }
 
+    public static void Main() {
+        var g = new Grafo();
+        g.Add(0, 1, -1);
+        g.Add(0, 2, 4);
+        g.Add(1, 2, 3);
+        g.Add(1, 3, 2);
+        g.Add(1, 4, 2);
+        g.Add(4, 3, -3);
 
+        var (dist, pred) = BellmanFord(g, 0);
 
-
-        
-      //
+        for (int i = 0; i < dist.Length; i++) {
+            Console.WriteLine("v" + i + ": d=" + dist[i] + " p=" + pred[i]);
+        }
     }
 }
